e2206e2a36df87e25e5fda1c55575781ba84ef38
Kit Jasper Pasayloon Garcia <121322250+skillsek@users.noreply.github.com>
2024-02-12 00:07:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+function openEnvelope() {
+  var envelope = document.querySelector('.envelope');
+  var message = document.querySelector('.message');
+  envelope.style.width = "200px";
+  message.style.display = "block";
+}
+
+
 onload = () => {
   document.body.classList.remove("container");
 };
+
+
